9c0275d57516020672159b623ca3ca8f8c15cacb
gavin <gavinberryman@gmail.com>
2024-08-26 14:40:54 -0400

    Updates

--- js/iovox.js
+++ js/iovox.js
@@ -5,7 +5,7 @@ document.getElementsByTagName("head")[0].appendChild(iovoxApi);
 
 window.iovoxApiConfig = {
 
-    accessKey:"c0f5c6c2f259d362983d2f3ca653c186cdfd6ac7",
+    accessKey:"77acee60a413aac3aa203f9a7859b4b3e50a07f1",
     actionKey: "89479a09d55c162bd76746ff2dcbbb85213e31fe",
     globalNumberFormat: "xxx-xxx-xxxx",
     sea: true,
@@ -18,7 +18,7 @@ window.iovoxApiConfig = {
 window.iovoxApiConfig.requestLinks = {
 
     items:[
-        {linkId:"__ROTATING__", selector:"[phone-selector]", poolId:"reverbPool", dialString: "1"},
+        {linkId:"__ROTATING__", selector:"[phone-selector]", poolId:"reverbPool", dialString: "1"}
 
     ]
 
