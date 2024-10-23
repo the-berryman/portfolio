69f888055aa4db268629866b6105b6516bf1ab12
gavin <gavinberryman@gmail.com>
2024-10-23 16:04:30 -0400

    Updates

--- js/iovox.js
+++ js/iovox.js
@@ -6,7 +6,7 @@ iovoxApi.src = "https://cdn.iovox.com/rest/v1/dni.js"
 window.iovoxApiConfig = {
 
     accessKey:"77acee60a413aac3aa203f9a7859b4b3e50a07f1",
-    actionKey: "89479a09d55c162bd76746ff2dcbbb85213e31fe",
+    actionKey: "4371ddd444e6c5d679b5b6a26c3657dc8e23b979",
     globalNumberFormat: "xxx-xxx-xxxx",
     sea: true,
     manualNumberUpdate:false
